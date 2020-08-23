3f0c8a98f93b8d95c644c862055bae334b2dc39a
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-08-23 14:38:56 +0200

    refactorization #2

--- life/Main.java
+++ life/Main.java
@@ -8,136 +8,34 @@ public class Main {
 
     private static final Scanner SCAN = new Scanner(System.in);
 
-    public enum Direction {
-        NORTH(0, 1),
-        NORTH_EAST(1, 1),
-        EAST(1, 0),
-        SOUTH_EAST(1, -1),
-        SOUTH(0, -1),
-        SOUTH_WEST(-1, -1),
-        WEST(-1, 0),
-        NORTH_WEST(-1, 1);
-        final int dx;
-        final int dy;
-        Direction(int dx, int dy) {
-            this.dx = dx;
-            this.dy = dy;
-        }
-    }
-
     public static void main(String[] args) {
 
         int size = SCAN.nextInt();
         //long seed = SCAN.nextLong();
         //int generations = SCAN.nextInt();
-        int generations = 10;
+        int generations = 100;
 
-        boolean[][] current = generateRandomMatrix(size);
-        boolean[][] next = current.clone();
+        Universe universe = new Universe();
+        universe.setCurrent(UniverseManager.generateRandomMatrix(size));
+        universe.shallowCopyCurrentToNext();
 
-        print(current, -1);
         for (int generation = 0; generation < generations; generation++) {
-            current = Arrays.stream(next).map(boolean[]::clone).toArray(boolean[][]::new); // copy
-            for (int x = 0; x < current.length; x++) {
-                for (int y = 0; y < current.length; y++) {
-                    switch (neighbourCounter(current, x, y)) {
+            universe.deepCopyNextToCurrent();
+            for (int x = 0; x < universe.getSize(); x++) {
+                for (int y = 0; y < universe.getSize(); y++) {
+                    switch (universe.getNeighbourCounter(x, y)) {
                         case 3:
-                            next[x][y] = true; // reborn
+                            universe.setNext(x, y, true); // reborn
                             break;
                         case 2:
-                            next[x][y] = current[x][y]; // survive
+                            universe.setNext(x, y, universe.getCurrent(x, y)); // survive
                             break;
                         default:
-                            next[x][y] = false; // dead
+                            universe.setNext(x, y, false); // dead
                     }
                 }
             }
-            print(next, generation);
+            UniverseManager.print(universe, generation);
         }
-
-    }
-
-    /** good */
-    private static boolean[][] generateRandomMatrix(int size, long seed) {
-        boolean[][] array = new boolean[size][size];
-        Random random = new Random(seed);
-        for (int y = array.length - 1; y >= 0; y--) {
-            for (int x = 0; x < array.length; x++) {
-                array[x][y] = random.nextBoolean();
-            }
-        }
-        return array;
-    }
-
-    private static boolean[][] generateRandomMatrix(int size) {
-        boolean[][] array = new boolean[size][size];
-        Random random = new Random();
-        for (int y = array.length - 1; y >= 0; y--) {
-            for (int x = 0; x < array.length; x++) {
-                array[x][y] = random.nextBoolean();
-            }
-        }
-        return array;
-    }
-
-    /** good */
-    private static void print(boolean[][] array, int generation) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            //e.printStackTrace();
-        }
-
-        System.out.println("Generation #" + (generation + 2));
-        System.out.println("Alive #" + getAlive(array));
-        for (int y = array.length - 1; y >= 0; y--) {
-            for (boolean[] booleans : array) {
-                System.out.print(booleans[y] ? "O" : " ");
-            }
-            System.out.println();
-        }
-    }
-
-    private static int getAlive(boolean[][] array) {
-        int counter = 0;
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                if (array[i][j]) {
-                    counter++;
-                }
-            }
-        }
-        return counter;
-    }
-
-
-    /** good */
-    private static int neighbourCounter(boolean[][] array, int x, int y) {
-        int counter = 0;
-        for (Direction dir: Direction.values()) {
-            if (getNeighbour(dir, array, x, y)) {
-                counter++;
-            }
-        }
-        return counter;
-    }
-
-    /** good */
-    private static boolean getNeighbour(Direction dir, boolean[][] array, int x, int y) {
-        x += dir.dx;
-        y += dir.dy;
-        if (y < 0) {
-            y = array.length - 1;
-        }
-        if (x < 0) {
-            x = array.length - 1;
-        }
-        if (y >= array.length) {
-            y = 0;
-        }
-        if (x >= array.length) {
-            x = 0;
-        }
-        return array[x][y];
     }
 }
--- life/Universe.java
+++ life/Universe.java
@@ -1,2 +1,94 @@
-package life;public class Universe {
+package life;
+
+import java.util.Arrays;
+
+public class Universe {
+
+    private boolean[][] current;
+    private boolean[][] next;
+
+    /** deep copy next to current */
+    public void deepCopyNextToCurrent() {
+        current = Arrays.stream(next).map(boolean[]::clone).toArray(boolean[][]::new);
+    }
+
+    public void shallowCopyCurrentToNext() {
+        next = current.clone();
+    }
+
+    public int getNeighbourCounter(int x, int y) {
+        int counter = 0;
+        for (Direction direction : Direction.values()) {
+            if (getNeighbour(direction, x, y)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private boolean getNeighbour(Direction direction, int x, int y) {
+        x += direction.dx;
+        y += direction.dy;
+        if (y < 0) {
+            y = current.length - 1;
+        }
+        if (x < 0) {
+            x = current.length - 1;
+        }
+        if (y >= current.length) {
+            y = 0;
+        }
+        if (x >= current.length) {
+            x = 0;
+        }
+        return current[x][y];
+    }
+
+    public int getAliveCounter() {
+        int counter = 0;
+        for (boolean[] booleans : current) {
+            for (int i = 0; i < current.length; i++) {
+                if (booleans[i]) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
+
+    public int getSize() {
+        return current.length;
+    }
+
+    public boolean[][] getCurrent() {
+        return current;
+    }
+
+    public boolean[][] getNext() {
+        return next;
+    }
+
+    public boolean getCurrent(int x, int y) {
+        return current[x][y];
+    }
+
+    public boolean getNext(int x, int y) {
+        return next[x][y];
+    }
+
+    public void setCurrent(boolean[][] current) {
+        this.current = current;
+    }
+
+    public void setNext(boolean[][] next) {
+        this.next = next;
+    }
+
+    public void setCurrent(int x, int y, boolean value) {
+        current[x][y] = value;
+    }
+
+    public void setNext(int x, int y, boolean value) {
+        next[x][y] = value;
+    }
 }
