7b7f88f8644ce8580bd3c364877afcddc2a23d12
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-08-28 18:50:08 +0200

    encryptdecrypt stage 4/6

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -4,17 +4,40 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static final Scanner SCAN = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        switch (SCAN.nextLine()) {
-            case "enc":
-                System.out.println(crypt(SCAN.nextLine(), SCAN.nextInt(), false));
-                break;
-            case "dec":
-                System.out.println(crypt(SCAN.nextLine(), SCAN.nextInt(), true));
-                break;
+    enum Mode {
+        ENC(false), DEC(true);
+        boolean isReading;
+        Mode(boolean isReading) {
+            this.isReading = isReading;
         }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        // init
+        Mode mode = Mode.ENC;
+        int key = 0;
+        String data = "";
+
+        // read input
+        try {
+            for (int i = 0; i < args.length; i++) {
+                if ("-mode".equals(args[i])) {
+                    mode = Mode.valueOf(args[i + 1].toUpperCase());
+                }
+                else if ("-key".equals(args[i])) {
+                    key = Integer.parseInt(args[i + 1]);
+                }
+                else if ("-data".equals(args[i])) {
+                    data = args[i + 1];
+                }
+            }
+        } catch (Exception e) {
+            throw new Exception("Input error");
+        }
+
+        // logic
+        System.out.println(crypt(data, key, mode.isReading));
     }
 
     private static String crypt(String text, int key, boolean isReading) {
