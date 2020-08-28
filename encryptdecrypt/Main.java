54a3c2fceeada48ca8ba8ccbdd1bd1808a6a5c9f
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-08-28 16:22:39 +0200

    encryptdecrypt stage 3/6

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -4,29 +4,24 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static final int MIN_CHAR = 'a'; // 97
-    public static final int MAX_CHAR = 'z'; // 122
     public static final Scanner SCAN = new Scanner(System.in);
 
     public static void main(String[] args) {
-        final int range = MAX_CHAR - MIN_CHAR + 1;
-        String text = SCAN.nextLine();
-        int key = SCAN.nextInt() % range;
-        for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (isCharInRange(c)) {
-                c += key;
-                c -= isCharInRange(c) ? 0 : range;
-            }
-            System.out.print(c);
+        switch (SCAN.nextLine()) {
+            case "enc":
+                System.out.println(crypt(SCAN.nextLine(), SCAN.nextInt(), false));
+                break;
+            case "dec":
+                System.out.println(crypt(SCAN.nextLine(), SCAN.nextInt(), true));
+                break;
         }
     }
 
-    private static boolean isCharInRange(char c) {
-        return (c >= MIN_CHAR && c <= MAX_CHAR);
-    }
-
-    private static char codeCharCorrespondingPosition(char c) {
-        return isCharInRange(c) ? (char) (MAX_CHAR + MIN_CHAR - c) : c;
+    private static String crypt(String text, int key, boolean isReading) {
+        StringBuilder builder = new StringBuilder(text);
+        for (int i = 0; i < builder.length(); i++) {
+            builder.setCharAt(i, (char) (builder.charAt(i) + (isReading ? - key : key)));
+        }
+        return builder.toString();
     }
 }
