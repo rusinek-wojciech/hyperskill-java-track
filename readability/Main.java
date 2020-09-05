fb1c68ad348b0075cd2d6bf4474e4d9126822b1b
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-05 16:12:59 +0200

    readability - stage 2/4

--- readability/Main.java
+++ readability/Main.java
@@ -1,7 +1,15 @@
 package readability;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new java.util.Scanner(System.in).nextLine().length() > 100 ? "HARD" : "EASY");
+        String[] sentences = Pattern.compile("[?!.]").split(new Scanner(System.in).nextLine());
+        int sum = 0;
+        for (String sentence : sentences) {
+            sum += sentence.split(" ").length;
+        }
+        System.out.println((sum / sentences.length) > 10 ? "HARD" : "EASY");
     }
 }
