92c90965682f112cebe53c341ac41c8ddec415cc
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-05 13:24:58 +0200

    readability - stage 1/4

---
+++ readability/Main.java
@@ -0,0 +1,7 @@
+package readability;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new java.util.Scanner(System.in).nextLine().length() > 100 ? "HARD" : "EASY");
+    }
+}
