cd29ba884ab266e04ea547aadc1b51901b97be06
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-07 01:12:16 +0200

    converter - stage 4/6

--- converter/Main.java
+++ converter/Main.java
@@ -7,18 +7,10 @@ public class Main {
     final static private Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int x = SCANNER.nextInt();
-        int radix = SCANNER.nextInt();
-        switch (radix) {
-            case 2:
-                System.out.println("0b" + Long.toString(x, 2));
-                break;
-            case 8:
-                System.out.println("0" + Long.toString(x, 8));
-                break;
-            case 16:
-                System.out.println("0x" + Long.toString(x, 16));
-                break;
-        }
+        int inRadix = SCANNER.nextInt();
+        int num = SCANNER.nextInt();
+        int outRadix = SCANNER.nextInt();
+        num = inRadix <= 1 ? String.valueOf(num).length() : Integer.parseInt(String.valueOf(num), inRadix);
+        System.out.println(outRadix <= 1 ? "1".repeat(num) : Integer.toString(num, outRadix));
     }
 }
