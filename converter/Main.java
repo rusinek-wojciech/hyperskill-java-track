d3f5af54bcc965af2e5df3273cc346732fb14bb4
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-11 21:11:40 +0200

    converter stage 6/6

--- converter/Main.java
+++ converter/Main.java
@@ -1,40 +1,57 @@
 package converter;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
 
     final static private Scanner SCANNER = new Scanner(System.in);
+    private static boolean error = false;
 
     public static void main(String[] args) {
 
         // input
-        int inRadix = SCANNER.nextInt();
-        String input = SCANNER.next();
-        int outRadix = SCANNER.nextInt();
-
-        // first stage
-        String[] number = input.split("\\.");
-        int integer = inRadix <= 1 ? number[0].length() : Integer.parseInt(number[0], inRadix);
-        double fractional = 0.0;
-        if (number.length == 2) {
-            for (int i = 0; i < number[1].length(); i++) {
-                fractional += Character.getNumericValue(number[1].charAt(i)) / Math.pow(inRadix, i + 1);
-            }
+        int inRadix = 0;
+        String input = null;
+        int outRadix = 0;
+        try {
+            inRadix = SCANNER.nextInt();
+            input = SCANNER.next();
+            outRadix = SCANNER.nextInt();
+            error = inRadix < 1 || inRadix > 36 || outRadix < 1 || outRadix > 36;
+        } catch (Exception e) {
+            error = true;
         }
+        if (!error) {
 
-        // second stage
-        StringBuilder builder = new StringBuilder(Integer.toString(integer, outRadix) + ".");
-        for (int i = 0; i < 5; i++) {
-            fractional *= outRadix;
-            int temp = (int) fractional;
-            if (temp >= 10) {
-                builder.append((char) (temp + 87));
-            } else {
-                builder.append(temp);
+            // first stage
+            String[] number = input.split("\\.");
+            int integer = inRadix <= 1 ? number[0].length() : Integer.parseInt(number[0], inRadix);
+            double fractional = 0.0;
+            if (number.length == 2) {
+                for (int i = 0; i < number[1].length(); i++) {
+                    fractional += Character.getNumericValue(number[1].charAt(i)) / Math.pow(inRadix, i + 1);
+                }
             }
-            fractional -= temp;
+
+            // second stage
+            StringBuilder builder = new StringBuilder(Integer.toString(integer, outRadix) + ".");
+            for (int i = 0; i < 5; i++) {
+                fractional *= outRadix;
+                int temp = (int) fractional;
+                if (temp >= 10) {
+                    builder.append((char) (temp + 87));
+                } else {
+                    builder.append(temp);
+                }
+                fractional -= temp;
+            }
+            System.out.println(outRadix <= 1 ? "1".repeat(integer) : builder.toString());
+
+        } else {
+            System.out.println("error");
         }
-        System.out.println(outRadix <= 1 ? "1".repeat(integer) : builder.toString());
     }
 }
