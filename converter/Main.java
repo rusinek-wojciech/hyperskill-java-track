1289e1ac41f54096763b94547e9cb27d5cd9a863
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-11 20:55:27 +0200

    converter stage 5/6

--- converter/Main.java
+++ converter/Main.java
@@ -7,10 +7,34 @@ public class Main {
     final static private Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        // input
         int inRadix = SCANNER.nextInt();
-        int num = SCANNER.nextInt();
+        String input = SCANNER.next();
         int outRadix = SCANNER.nextInt();
-        num = inRadix <= 1 ? String.valueOf(num).length() : Integer.parseInt(String.valueOf(num), inRadix);
-        System.out.println(outRadix <= 1 ? "1".repeat(num) : Integer.toString(num, outRadix));
+
+        // first stage
+        String[] number = input.split("\\.");
+        int integer = inRadix <= 1 ? number[0].length() : Integer.parseInt(number[0], inRadix);
+        double fractional = 0.0;
+        if (number.length == 2) {
+            for (int i = 0; i < number[1].length(); i++) {
+                fractional += Character.getNumericValue(number[1].charAt(i)) / Math.pow(inRadix, i + 1);
+            }
+        }
+
+        // second stage
+        StringBuilder builder = new StringBuilder(Integer.toString(integer, outRadix) + ".");
+        for (int i = 0; i < 5; i++) {
+            fractional *= outRadix;
+            int temp = (int) fractional;
+            if (temp >= 10) {
+                builder.append((char) (temp + 87));
+            } else {
+                builder.append(temp);
+            }
+            fractional -= temp;
+        }
+        System.out.println(outRadix <= 1 ? "1".repeat(integer) : builder.toString());
     }
 }
