2ab27c07471e53253dc735c403f9b5e6234ef512
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-14 18:42:02 +0200

    refactoring after processor stage 4/6

--- processor/MatrixManager.java
+++ processor/MatrixManager.java
@@ -2,17 +2,6 @@ package processor;
 
 public class MatrixManager {
 
-    public static Matrix add(Matrix m1, Matrix m2) {
-        if (m1.getRows() == m2.getRows() && m1.getColumns() == m2.getColumns()) {
-            return new Matrix(m1.getRows(), m2.getColumns(), (r, c) -> m1.get(r, c) + m2.get(r, c));
-        }
-        return null;
-    }
-
-    public static Matrix scalarMultiply(Matrix m, int multiplier) {
-        return new Matrix(m.getRows(), m.getColumns(), (r, c) -> m.get(r, c) * multiplier);
-    }
-
     public static Matrix multiply(Matrix m1, Matrix m2) {
         if (m1.getColumns() == m2.getRows()) {
             return new Matrix(m1.getRows(), m2.getColumns(), (r, c) -> {
@@ -26,19 +15,34 @@ public class MatrixManager {
         return null;
     }
 
+    public static Matrix add(Matrix m1, Matrix m2) {
+        return (m1.getRows() == m2.getRows() && m1.getColumns() == m2.getColumns()) ?
+                new Matrix(m1.getRows(), m2.getColumns(),
+                        (r, c) -> m1.get(r, c) + m2.get(r, c)) : null;
+    }
+
+    public static Matrix scalarMultiply(Matrix m, int multiplier) {
+        return new Matrix(m.getRows(), m.getColumns(),
+                (r, c) -> m.get(r, c) * multiplier);
+    }
+
     public static Matrix transposeOverMainDiagonal(Matrix m) {
-        return new Matrix(m.getColumns(), m.getRows(), (r, c) -> m.get(c, r));
+        return new Matrix(m.getColumns(), m.getRows(),
+                (r, c) -> m.get(c, r));
     }
 
     public static Matrix transposeOverSideDiagonal(Matrix m) {
-        return new Matrix(m.getColumns(), m.getRows(), (r, c) -> m.get(m.getColumns() - c - 1, m.getRows() - r - 1));
+        return new Matrix(m.getColumns(), m.getRows(),
+                (r, c) -> m.get(m.getColumns() - c - 1, m.getRows() - r - 1));
     }
 
     public static Matrix transposeByVerticalLine(Matrix m) {
-        return new Matrix(m.getRows(), m.getColumns(), (r, c) -> m.get(r, m.getColumns() - c - 1));
+        return new Matrix(m.getRows(), m.getColumns(),
+                (r, c) -> m.get(r, m.getColumns() - c - 1));
     }
 
     public static Matrix transposeByHorizontalLine(Matrix m) {
-        return new Matrix(m.getRows(), m.getColumns(), (r, c) -> m.get(m.getRows() - r - 1, c));
+        return new Matrix(m.getRows(), m.getColumns(),
+                (r, c) -> m.get(m.getRows() - r - 1, c));
     }
 }
