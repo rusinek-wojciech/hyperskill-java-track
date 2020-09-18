f3149eb223f63df5617858ab4a543546319f43b7
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-18 17:47:30 +0200

    correcter - stage 4/5

--- correcter/Main.java
+++ correcter/Main.java
@@ -16,41 +16,138 @@ public class Main {
     private static final String DECODE_FILE = "decoded.txt";
 
     public static void main(String[] args) throws IOException {
-
-        String mode = null;
-        String data = null;
-        String encoded = null;
-        String error = null;
-        String decoded = null;
-
         System.out.print("Write a mode: ");
-        mode = SCANNER.next();
+        String mode = SCANNER.next();
         System.out.println();
         switch (mode.toLowerCase()) {
             case "encode":
-                data = formatToString(readFromFile(SEND_FILE));
-                encoded = encode(data);
-                saveToFileAsByte(ENCODE_FILE, formatToByte(encoded));
-                showData(data, SEND_FILE);
-                showData(encoded, ENCODE_FILE);
+                encodeInstruction();
                 break;
             case "send":
-                data = formatToString(readFromFile(ENCODE_FILE));
-                error = generateError(data);
-                saveToFileAsByte(RECEIVE_FILE, formatToByte(error));
-                showData(data, ENCODE_FILE);
-                showData(error, RECEIVE_FILE);
+                sendInstruction();
                 break;
             case "decode":
-                data = formatToString(readFromFile(RECEIVE_FILE));
-                decoded = decode(data);
-                saveToFileAsCharacter(DECODE_FILE, formatToByte(decoded));
-                showData(data, RECEIVE_FILE);
-                showData(decoded, DECODE_FILE);
+                decodeInstruction();
                 break;
             default:
         }
     }
+
+    /** ===================================================================== */
+
+    private static void encodeInstruction() throws IOException {
+        String data = getBinFromByte(readFromFile(SEND_FILE));
+        String encoded = encode(data);
+        saveToFileAsByte(ENCODE_FILE, getByteFromBinString(encoded));
+        System.out.println(SEND_FILE + ":");
+        System.out.println("text view: " + getTextStringFromBinString(data));
+        System.out.println("hex view: " + getBinWithSpacesFromBin(data));
+        System.out.println("bin view: " + getBinWithSpacesFromBin(data) + "\n");
+        System.out.println(ENCODE_FILE + ":");
+        System.out.println("expand: " + getParityString(getBinWithSpacesFromBin(encoded)));
+        System.out.println("parity: " + getBinWithSpacesFromBin(encoded));
+        System.out.println("hex view: " + getBinWithSpacesFromBin(encoded));
+    }
+
+    private static void sendInstruction() throws IOException {
+        String data = getBinFromByte(readFromFile(ENCODE_FILE));
+        String error = send(data);
+        saveToFileAsByte(RECEIVE_FILE, getByteFromBinString(error));
+        System.out.println(ENCODE_FILE + ":");
+        System.out.println("hex view: " + getHexFromBin(data));
+        System.out.println("bin view: " + getBinWithSpacesFromBin(data) + "\n");
+        System.out.println(RECEIVE_FILE + ":");
+        System.out.println("bin view: " + getBinWithSpacesFromBin(error));
+        System.out.println("hex view: " + getHexFromBin(error));
+    }
+
+    private static void decodeInstruction() throws IOException {
+        String data = getBinFromByte(readFromFile(RECEIVE_FILE));
+        String decoded = decode(data);
+        String decodedRemove = remove(decoded);
+        saveToFileAsByte(DECODE_FILE, getByteFromBinString(decodedRemove));
+        System.out.println(RECEIVE_FILE + ":");
+        System.out.println("hex view: " + getHexFromBin(data));
+        System.out.println("bin view: " + getBinWithSpacesFromBin(data) + "\n");
+        System.out.println(DECODE_FILE + ":");
+        System.out.println("correct: " + getBinWithSpacesFromBin(encode(decoded)));
+        System.out.println("decode: " + getBinWithSpacesFromBin(decoded));
+        System.out.println("remove: " + getBinWithSpacesFromBin(decodedRemove));
+        System.out.println("hex view: " + getHexFromBin(decodedRemove));
+        System.out.println("text view: " + getTextStringFromBinString(decodedRemove));
+    }
+
+    /** ===================================================================== */
+
+    private static String getHexFromBin(String data) {
+        StringBuilder result = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < data.length(); i++) {
+            if (i % 4 == 0 && i != 0) {
+                int decimal = Integer.parseInt(builder.toString(), 2);
+                String hex = Integer.toString(decimal, 16).toUpperCase();
+                result.append(hex);
+                if (i % 8 == 0) {
+                    result.append(" ");
+                }
+                builder.delete(0, 4);
+            }
+            builder.append(data.charAt(i));
+        }
+        int decimal = Integer.parseInt(builder.toString(), 2);
+        String hex = Integer.toString(decimal, 16).toUpperCase();
+        result.append(hex);
+        return result.toString();
+    }
+
+    private static String getBinWithSpacesFromBin(String data) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < data.length(); i++) {
+            if (i % 8 == 0 && i != 0) {
+                result.append(" ");
+            }
+            result.append(data.charAt(i));
+        }
+        return result.toString();
+    }
+
+    private static String getParityString(String data) {
+        StringBuilder result = new StringBuilder(data);
+        for (int i = 0; i < data.length(); i += (Byte.SIZE + 1)) {
+            result.setCharAt(i + 6, '.');
+            result.setCharAt(i + 7, '.');
+        }
+        return result.toString();
+    }
+
+    private static String getBinFromByte(byte[] data) {
+        StringBuilder result = new StringBuilder();
+        for (byte datum : data) {
+            for (int j = Byte.SIZE - 1; j >= 0 ; j--) {
+                result.append((datum >> j) & 1);
+            }
+        }
+        return result.toString();
+    }
+
+    private static String getTextStringFromBinString(String data) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < data.length(); i += Byte.SIZE) {
+            result.append((char) Integer.parseInt(data.substring(i, Byte.SIZE + i), 2));
+        }
+        return result.toString();
+    }
+
+    private static byte[] getByteFromBinString(String data) {
+        String[] bytes = data.split("(?<=\\G.{8})");
+        byte[] result = new byte[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            result[i] = (byte) Integer.parseInt(bytes[i], 2);
+        }
+        return result;
+    }
+
+    /** ===================================================================== */
 
     private static String decode(String data) {
         StringBuilder result = new StringBuilder();
@@ -87,16 +184,6 @@ public class Main {
         return result.toString();
     }
 
-    private static String formatToString(byte[] data) {
-        StringBuilder result = new StringBuilder();
-        for (byte datum : data) {
-            for (int j = Byte.SIZE - 1; j >= 0 ; j--) {
-                result.append((datum >> j) & 1);
-            }
-        }
-        return result.toString();
-    }
-
     private static String encode(String data) {
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < data.length(); i++) {
@@ -108,11 +195,27 @@ public class Main {
                 result.append(a ^ b ^ c).append(a ^ b ^ c);
             }
         }
-        int missingSize = 3 * data.length() - result.length();
-        return result.append("0".repeat(missingSize)).toString();
+        if (data.length() % 3 == 2) {
+            byte a = Byte.parseByte(String.valueOf(data.charAt(data.length() - 2)));
+            byte b = Byte.parseByte(String.valueOf(data.charAt(data.length() - 1)));
+            byte c = 0;
+            result.append("00");
+            result.append(a ^ b ^ c).append(a ^ b ^ c);
+        }
+        if (data.length() % 3 == 1) {
+            byte a = Byte.parseByte(String.valueOf(data.charAt(data.length() - 1)));
+            byte b = 0;
+            byte c = 0;
+            result.append("0000");
+            result.append(a ^ b ^ c).append(a ^ b ^ c);
+        }
+        while (result.length() % Byte.SIZE != 0) {
+            result.append("0");
+        }
+        return result.toString();
     }
 
-    private static String generateError(String data) {
+    private static String send(String data) {
         Random random = new Random();
         StringBuilder result = new StringBuilder(data);
         for (int i = Byte.SIZE - 1; i < data.length(); i += Byte.SIZE) {
@@ -122,65 +225,23 @@ public class Main {
         return result.toString();
     }
 
+    private static String remove(String data) {
+        StringBuilder result = new StringBuilder(data);
+        while (result.length() % Byte.SIZE  != 0) {
+            result.deleteCharAt(result.length() - 1);
+        }
+        return result.toString();
+    }
+
+    /** ===================================================================== */
+
     private static byte[] readFromFile(String fileName) throws IOException {
         return Files.readAllBytes(Paths.get(fileName));
-    }
-
-    private static byte[] formatToByte(String data) {
-        String[] bytes = data.split("(?<=\\G.{8})");
-        byte[] result = new byte[bytes.length];
-        for (int i = 0; i < bytes.length; i++) {
-            result[i] = (byte) Integer.parseInt(bytes[i], 2);
-        }
-        return result;
-    }
-
-    private static void showData(String data, String file) {
-        System.out.println(file + ":");
-        showHexView(data);
-        showBinView(data);
-    }
-
-    private static void showBinView(String data) {
-        System.out.print("bin view: ");
-        for (int i = 0; i < data.length(); i++) {
-            if (i % 8 == 0 && i != 0) {
-                System.out.print(" ");
-            }
-            System.out.print(data.charAt(i));
-        }
-        System.out.println();
-    }
-
-    private static void showHexView(String data) {
-        System.out.print("hex view: ");
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < data.length(); i++) {
-            if (i % 4 == 0 && i != 0) {
-                int decimal = Integer.parseInt(builder.toString(), 2);
-                String hex = Integer.toString(decimal, 16).toUpperCase();
-                System.out.print(hex);
-                if (i % 8 == 0) {
-                    System.out.print(" ");
-                }
-                builder.delete(0, 4);
-            }
-            builder.append(data.charAt(i));
-        }
-        System.out.println();
     }
 
     private static void saveToFileAsByte(String fileName, byte[] data) throws IOException {
         FileOutputStream output = new FileOutputStream(new File(fileName));
         output.write(data);
         output.close();
-    }
-
-    private static void saveToFileAsCharacter(String fileName, byte[] data) throws IOException {
-        String text = new String(data, StandardCharsets.UTF_8);
-        char[] array = text.toCharArray();
-        FileWriter writer = new FileWriter(fileName);
-        writer.write(array);
-        writer.close();
     }
 }
