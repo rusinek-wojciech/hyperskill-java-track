11baf4d701ccfe4c50bda613a6cf5d4486a2b36e
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-26 13:31:30 +0200

    solver - stage 2/5

--- hard/solver/Main.java
+++ hard/solver/Main.java
@@ -7,8 +7,12 @@ public class Main {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double a = SCANNER.nextDouble();
-        double b = SCANNER.nextDouble();
-        System.out.println(b / a);
+        double [] h = new double[6];
+        for (int i = 0; i < h.length; i++) {
+            h[i] = SCANNER.nextDouble();
+        }
+        double y = (h[0] * h[5] - h[2] * h[3]) / (h[0] * h[4] - h[1] * h[3]);
+        double x = (h[2] - h[1] * y) / h[0];
+        System.out.println(x + " " + y);
     }
 }
