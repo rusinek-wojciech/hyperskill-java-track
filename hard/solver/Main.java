60a4b44e32e7fede91457fa78f95b858a2250931
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-09-26 13:18:55 +0200

    solver - stage 1/5

---
+++ hard/solver/Main.java
@@ -0,0 +1,14 @@
+package hard.solver;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double a = SCANNER.nextDouble();
+        double b = SCANNER.nextDouble();
+        System.out.println(b / a);
+    }
+}
