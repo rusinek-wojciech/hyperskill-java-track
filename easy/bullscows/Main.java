9022ec1003d150872413d9fdd14e1ce1481985a6
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-10-09 14:02:33 +0200

    bullscows - stage 6/7

--- easy/bullscows/Main.java
+++ easy/bullscows/Main.java
@@ -6,31 +6,45 @@ import java.util.Scanner;
 public class Main {
 
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final int MAX_RANGE = 36;
 
     public static void main(String[] args) {
-        System.out.println("Please, enter the secret code's length:");
-        int size = SCANNER.nextInt();
-        if (size > 10) {
-            System.out.println("Error: can't generate a secret number with a length of " + size + " because there aren't enough unique digits.");
-            return;
-        }
-        String number = getRandom(size);
+
+        System.out.println("Input the length of the secret code: ");
+        int length = SCANNER.nextInt();
+        System.out.println("Input the number of possible symbols in the code: ");
+        int range = SCANNER.nextInt();
+
+        String number = getRandom(length, range);
+        System.out.println(getPrepareString(length, range));
         System.out.println("Okay, let's start a game!");
+
         int round = 1;
         while (true) {
             System.out.println("Turn " + (round++) + ":");
             String input = SCANNER.next();
             final int bulls = getBulls(input, number);
             final int cows = getCows(input, number);
-            System.out.println(getString(bulls, cows));
-            if (bulls == size) {
+            System.out.println(getGradeString(bulls, cows));
+            if (bulls == length) {
                 System.out.println("Congratulations! You guessed the secret code.");
                 break;
             }
         }
     }
 
-    private static String getString(int bulls, int cows) {
+    private static String getPrepareString(int length, int range ) {
+        StringBuilder builder = new StringBuilder("The secret is prepared: ");
+        builder.append("*".repeat(length)).append(" ");
+        if (range <= 10) {
+            builder.append("(0-").append(range - 1).append(").");
+        } else {
+            builder.append("(0-9, a-").append((char)(range + 86)).append(").");
+        }
+        return builder.toString();
+    }
+
+    private static String getGradeString(int bulls, int cows) {
         StringBuilder builder = new StringBuilder();
         builder.append("Grade: ");
         if (bulls >= 1 && cows >= 1) {
@@ -72,15 +86,16 @@ public class Main {
         return counter;
     }
 
-    private static String getRandom(int size) {
+    private static String getRandom(int length, int range) {
         Random rand = new Random();
         StringBuilder number = new StringBuilder();
-        for (int i = 0; i < size; i++) {
-            int r = rand.nextInt(10);
-            if (number.indexOf(String.valueOf(r)) == -1) {
-                number.append(r);
+        for (int i = 0; i < length; i++) {
+            int r = rand.nextInt(range);
+            r += r < 10 ? 48 : 87; // 48 - 58, 97 - 123
+            if (number.indexOf(String.valueOf((char) r)) == -1) {
+                number.append((char) r);
             } else {
-                size++;
+                length++;
             }
         }
         return number.toString();
