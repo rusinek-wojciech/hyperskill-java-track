cefe591048231095af5989434cd238b082b09e46
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-10-10 23:51:10 +0200

    battleship - stage 2/5

--- medium/battleship/Main.java
+++ medium/battleship/Main.java
@@ -12,10 +12,42 @@ public class Main {
         Board board = new Board(BOARD_SIZE);
         System.out.println(board);
 
+        prepare(board);
+
+        System.out.println("\nThe game starts!\n");
+        System.out.println(board);
+
+        System.out.println("Take a shot!\n");
+
+        int x;
+        int y;
+        while (true) {
+
+            String shot = SCANNER.next();
+            x = board.getX(shot);
+            y = board.getY(shot);
+
+            if (isPositionCorrect(x, y)) {
+                break;
+            } else {
+                System.out.println("\nError! You entered the wrong coordinates! Try again: \n");
+            }
+        }
+        char sign = board.getPosition(x, y);
+        if (sign == Board.FOG_OF_WAR) {
+            System.out.println("You missed!");
+            board.setPosition(x, y, Board.MISS);
+        } else if (sign == Board.OUR_SHIP) {
+            System.out.println("You hit a ship!");
+            board.setPosition(x, y, Board.HIT);
+        }
+
+        System.out.println("\n" + board);
+    }
+
+    private static void prepare(Board board) {
         for (Ship ship : Ship.values()) {
-
             System.out.println("Enter the coordinates of " + ship + ":\n");
-
             while (true) {
 
                 String startPos = SCANNER.next();
@@ -41,6 +73,10 @@ public class Main {
         }
     }
 
+    private static boolean isPositionCorrect(int x, int y) {
+        return x >= 0 && x < BOARD_SIZE && y >= 0 && y < BOARD_SIZE;
+    }
+
     private static void setShips(int x1, int x2, int y1, int y2, Ship ship, Board board) {
         for (int x = Math.min(x1, x2); x <= Math.max(x1, x2); x++) {
             for (int y = Math.min(y1, y2); y <= Math.max(y1, y2); y++) {
@@ -55,7 +91,7 @@ public class Main {
                 for (Direction dir : Direction.values()) {
                     final int X = x + dir.x;
                     final int Y = y + dir.y;
-                    if (X >= 0 && X < BOARD_SIZE && Y >= 0 && Y < BOARD_SIZE) {
+                    if (isPositionCorrect(X, Y)) {
                         if (board.getPosition(X, Y) == Board.OUR_SHIP) {
                             return true;
                         }
