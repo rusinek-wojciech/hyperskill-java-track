0f5ad3e74ca0349699f42cd85485f66858886ce4
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-10 00:01:52 +0100

    changed gen algorithm

--- com/ikinsure/hyperskill/hard/maze/Maze.java
+++ com/ikinsure/hyperskill/hard/maze/Maze.java
@@ -20,14 +20,18 @@ public class Maze {
     }
 
     public void randomize() {
-        int h = (height + 2) / 2;
-        int w = (width + 2) / 2;
-        int size = h * w;
-        Graph graph = new Graph(size, 2 * size - h - w);
-        Random random = new Random();
 
+        // initialize
+        int h = ((height + 1) / 2) - 1;
+        int w = ((width + 1) / 2) - 1;
+        boolean isEven = width % 2 == 0;
+        int size = h * w;
+        Graph graph = new Graph(size + 2, 2 * size - h - w + 2);
+        Random random = new Random();
         int edgeIterator = 0;
-        for (int i = 0; i < graph.vertices; i++) { // create horizontal edges
+
+        // create horizontal edges
+        for (int i = 0; i < graph.vertices - 2; i++) {
             if ((i + 1) % w != 0) {
                 graph.edge[edgeIterator].source = i;
                 graph.edge[edgeIterator].destination = i + 1;
@@ -35,23 +39,50 @@ public class Maze {
                 edgeIterator++;
             }
         }
-        for (int i = 0; i < graph.vertices - w; i++) { // create vertical edges
+
+        // create vertical edges
+        for (int i = 0; i < graph.vertices - w - 2; i++) {
             graph.edge[edgeIterator].source = i;
             graph.edge[edgeIterator].destination = i + w;
             graph.edge[edgeIterator].weight = random.nextInt(100);
             edgeIterator++;
         }
-        Edge[] result = graph.kruskal(true); // calculate minimum spanning tree
-        for (Edge e : result) { // set passage where are edges
-            int jPosSrc = e.source % w;
-            int iPosSrc = e.source / w;
-            int jPosDest = e.destination % w;
-            int iPosDest = e.destination / w;
-            array[iPosSrc + iPosDest][jPosSrc + jPosDest] = Block.PASSAGE.id;
+
+        // starting and ending points (two last)
+        graph.edge[graph.edge.length - 2].source = size;
+        graph.edge[graph.edge.length - 2].destination = random.nextInt(h) * w;
+        graph.edge[graph.edge.length - 2].weight = 1;
+        graph.edge[graph.edge.length - 1].source = size + 1;
+        graph.edge[graph.edge.length - 1].destination = (random.nextInt(h) * w) + w - 1;
+        graph.edge[graph.edge.length - 1].weight = 1;
+
+        // set passage where are edges of minimum spanning tree
+        for (Edge e : graph.kruskal(true)) {
+            if (e.source == size) {
+                array[(e.destination / w) * 2 + 1][0] = Block.PASSAGE.id;
+            } else if (e.source == size + 1) {
+                int iPos = (e.destination / w) * 2 + 1;
+                array[iPos][width - 1] = Block.PASSAGE.id;
+                if (isEven) {
+                    array[iPos][width - 2] = Block.PASSAGE.id;
+                }
+            } else {
+                int iPos = (e.source / w) + (e.destination / w) + 1;
+                int jPos = (e.source % w) + (e.destination % w) + 1;
+                if (iPos < height && jPos < width) {
+                    array[iPos][jPos] = Block.PASSAGE.id;
+                }
+            }
         }
-        for (int i = 0; i < h; i++) { // set passage on vertices
+
+        // set passage on vertices
+        for (int i = 0; i < h; i++) {
             for (int j = 0; j < w; j++) {
-                array[i * 2][j * 2] = Block.PASSAGE.id;
+                int iPos = i * 2 + 1;
+                int jPos = j * 2 + 1;
+                if (iPos < height && jPos < width) {
+                    array[iPos][jPos] = Block.PASSAGE.id;
+                }
             }
         }
     }
@@ -59,12 +90,15 @@ public class Maze {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
+        builder.append("o ".repeat(width + 1)).append("\n");
         for (int i = 0; i < height; i++) {
+            builder.append("o");
             for (int j = 0; j < width; j++) {
                 builder.append(Block.findTextureById(array[i][j]));
             }
-            builder.append("\n");
+            builder.append("o\n");
         }
+        builder.append("o ".repeat(width + 1)).append("\n");
         return builder.toString();
     }
 }
