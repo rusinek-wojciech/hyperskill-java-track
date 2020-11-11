2289deebd2ee210c291e02dd67f0769cecd7a673
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-12 00:15:56 +0100

    cinema - stage 1/5

---
+++ com/ikinsure/hyperskill/easy/cinema/Cinema.java
@@ -0,0 +1,15 @@
+package com.ikinsure.hyperskill.easy.cinema;
+
+public class Cinema {
+    public static void main(String[] args) {
+        System.out.println("Cinema:\n" +
+                "  1 2 3 4 5 6 7 8\n" +
+                "1 S S S S S S S S\n" +
+                "2 S S S S S S S S\n" +
+                "3 S S S S S S S S\n" +
+                "4 S S S S S S S S\n" +
+                "5 S S S S S S S S\n" +
+                "6 S S S S S S S S\n" +
+                "7 S S S S S S S S");
+    }
+}
