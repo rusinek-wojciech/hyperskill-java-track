a47541b1e58f82e17bcc98a6ae93fda7cfb5224d
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-12 00:33:55 +0100

    cinema - stage 2/5

--- com/ikinsure/hyperskill/easy/cinema/Cinema.java
+++ com/ikinsure/hyperskill/easy/cinema/Cinema.java
@@ -1,15 +1,27 @@
 package com.ikinsure.hyperskill.easy.cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Cinema:\n" +
-                "  1 2 3 4 5 6 7 8\n" +
-                "1 S S S S S S S S\n" +
-                "2 S S S S S S S S\n" +
-                "3 S S S S S S S S\n" +
-                "4 S S S S S S S S\n" +
-                "5 S S S S S S S S\n" +
-                "6 S S S S S S S S\n" +
-                "7 S S S S S S S S");
+        System.out.println("Enter the number of rows:");
+        int rows = SCANNER.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seats = SCANNER.nextInt();
+
+        int income;
+        if (rows * seats <= 60) {
+            income = rows * seats * 10;
+        } else {
+            int half = rows / 2;
+            income = half * seats * 10;
+            income += (rows - half) * seats * 8;
+        }
+
+        System.out.println("Total income:");
+        System.out.println("$" + income);
     }
 }
