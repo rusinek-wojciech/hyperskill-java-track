2b1745783600bd9741c8f2e1764666824db4e57e
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-12 01:03:47 +0100

    cinema - stage 3/5

--- com/ikinsure/hyperskill/easy/cinema/Cinema.java
+++ com/ikinsure/hyperskill/easy/cinema/Cinema.java
@@ -7,21 +7,31 @@ public class Cinema {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         System.out.println("Enter the number of rows:");
         int rows = SCANNER.nextInt();
         System.out.println("Enter the number of seats in each row:");
-        int seats = SCANNER.nextInt();
+        int cols = SCANNER.nextInt();
 
-        int income;
-        if (rows * seats <= 60) {
-            income = rows * seats * 10;
+        Scene scene = new Scene(rows, cols);
+        System.out.println("\nCinema:");
+        System.out.println(scene);
+
+        System.out.println("Enter a row number:");
+        int y = SCANNER.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        int x = SCANNER.nextInt();
+
+        int price;
+        if (scene.getSeats() <= 60) {
+            price = 10;
         } else {
-            int half = rows / 2;
-            income = half * seats * 10;
-            income += (rows - half) * seats * 8;
+            price = y > rows / 2 ? 8 : 10;
         }
+        System.out.println("\nTicket price: $" + price);
 
-        System.out.println("Total income:");
-        System.out.println("$" + income);
+        scene.getPlaces()[y - 1][x - 1] = Scene.Code.BUSY.id;
+        System.out.println("\nCinema:");
+        System.out.println(scene);
     }
 }
