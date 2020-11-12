38a70b51e4a031c72fc9f30ad5bf0c0014d47b0a
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-12 01:12:56 +0100

    cinema - stage 4/5

--- com/ikinsure/hyperskill/easy/cinema/Cinema.java
+++ com/ikinsure/hyperskill/easy/cinema/Cinema.java
@@ -12,26 +12,39 @@ public class Cinema {
         int rows = SCANNER.nextInt();
         System.out.println("Enter the number of seats in each row:");
         int cols = SCANNER.nextInt();
-
         Scene scene = new Scene(rows, cols);
-        System.out.println("\nCinema:");
-        System.out.println(scene);
 
-        System.out.println("Enter a row number:");
-        int y = SCANNER.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int x = SCANNER.nextInt();
-
-        int price;
-        if (scene.getSeats() <= 60) {
-            price = 10;
-        } else {
-            price = y > rows / 2 ? 8 : 10;
+        boolean isRunning = true;
+        while (isRunning) {
+            switch (menu()) {
+                case 1:
+                    System.out.println("\nCinema:");
+                    System.out.print(scene);
+                    break;
+                case 2:
+                    System.out.println("\nEnter a row number:");
+                    int y = SCANNER.nextInt();
+                    System.out.println("Enter a seat number in that row:");
+                    int x = SCANNER.nextInt();
+                    int price;
+                    if (scene.getSeats() <= 60) {
+                        price = 10;
+                    } else {
+                        price = y > rows / 2 ? 8 : 10;
+                    }
+                    System.out.println("Ticket price: $" + price);
+                    scene.getPlaces()[y - 1][x - 1] = Scene.Code.BUSY.id;
+                    break;
+                default:
+                    isRunning = false;
+            }
         }
-        System.out.println("\nTicket price: $" + price);
+    }
 
-        scene.getPlaces()[y - 1][x - 1] = Scene.Code.BUSY.id;
-        System.out.println("\nCinema:");
-        System.out.println(scene);
+    private static int menu() {
+        System.out.println("\n1. Show the seats\n" +
+                "2. Buy a ticket\n" +
+                "0. Exit");
+        return SCANNER.nextInt();
     }
 }
