cb4f3704c2f042b0fd306a9579c76dfeb180a433
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-13 13:53:10 +0100

    sort - stage 3/6

--- com/ikinsure/hyperskill/hard/sorting/Main.java
+++ com/ikinsure/hyperskill/hard/sorting/Main.java
@@ -4,37 +4,79 @@ import java.util.*;
 
 public class Main {
 
-    private static final String TYPE = "-dataType";
-    private static final String LONG = "long";
-    private static final String LINE = "line";
-    private static final String WORD = "word";
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final DecimalFormat FORMAT = new DecimalFormat("#");
 
     public static void main(final String[] args) {
-        boolean isArg = args != null && args.length >= 2;
-        if (isArg && TYPE.equals(args[0]) && LONG.equals(args[1])) {
-            ArrayList<Long> list = new ArrayList<>();
-            while (SCANNER.hasNextLong()) {
-                list.add(SCANNER.nextLong());
+
+        // create map from args
+        LinkedHashMap<String, String> commands = new LinkedHashMap<>();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                if (args.length - 1 > i && !args[i + 1].startsWith("-")) {
+                    commands.put(args[i], args[i + 1]);
+                    i++;
+                } else {
+                    commands.put(args[i], null);
+                }
             }
-            printLong(list);
-        } else if (isArg && TYPE.equals(args[0]) && LINE.equals(args[1])) {
-            ArrayList<String> list = new ArrayList<>();
-            while (SCANNER.hasNextLine()) {
-                list.add(SCANNER.nextLine());
-            }
-            printLine(list);
+        }
+
+        // process commands
+        if (commands.containsKey("-sortIntegers")) {
+            sortIntegers();
         } else {
-            ArrayList<String> list = new ArrayList<>();
-            while (SCANNER.hasNext()) {
-                list.add(SCANNER.next());
+            if ("long".equals(commands.get("-dataType"))) {
+                dataTypeLong();
+            } else if ("line".equals(commands.get("-dataType"))) {
+                dataTypeLine();
+            } else {
+                dataTypeWord();
             }
-            printWord(list);
         }
     }
 
-    private static void printLong(ArrayList<Long> list) {
+    private static void sortIntegers() {
+        ArrayList<Long> list = new ArrayList<>();
+        while (SCANNER.hasNextLong()) {
+            list.add(SCANNER.nextLong());
+        }
+        printSortIntegers(list);
+    }
+
+    private static void dataTypeWord() {
+        ArrayList<String> list = new ArrayList<>();
+        while (SCANNER.hasNext()) {
+            list.add(SCANNER.next());
+        }
+        printDataTypeWord(list);
+    }
+
+    private static void dataTypeLong() {
+        ArrayList<Long> list = new ArrayList<>();
+        while (SCANNER.hasNextLong()) {
+            list.add(SCANNER.nextLong());
+        }
+        printDataTypeLong(list);
+    }
+
+    private static void dataTypeLine() {
+        ArrayList<String> list = new ArrayList<>();
+        while (SCANNER.hasNextLine()) {
+            list.add(SCANNER.nextLine());
+        }
+        printDataTypeLine(list);
+    }
+
+    private static void printSortIntegers(ArrayList<Long> list) {
+        Collections.sort(list);
+        System.out.println("Total numbers: " + list.size() + ".");
+        System.out.print("Sorted data: ");
+        list.forEach(i -> System.out.print(i + " "));
+        System.out.println();
+    }
+
+    private static void printDataTypeLong(ArrayList<Long> list) {
         long max = Collections.max(list);
         int freq = Collections.frequency(list, max);
         double percent = 100.0 * freq / list.size();
@@ -42,7 +84,7 @@ public class Main {
         System.out.println("The largest number: " + max + " (" + freq + " time(s), " + FORMAT.format(percent) + "%).");
     }
 
-    private static void printLine(ArrayList<String> list) {
+    private static void printDataTypeLine(ArrayList<String> list) {
         String max = Collections.max(list, Comparator.comparingInt(String::length));
         int freq = Collections.frequency(list, max);
         double percent = 100.0 * freq / list.size();
@@ -52,7 +94,7 @@ public class Main {
         System.out.println("(" + freq + " time(s), " + FORMAT.format(percent) + "%).");
     }
 
-    private static void printWord(ArrayList<String> list) {
+    private static void printDataTypeWord(ArrayList<String> list) {
         String max = Collections.max(list, Comparator.comparingInt(String::length));
         int freq = Collections.frequency(list, max);
         double percent = 100.0 * freq / list.size();
