eed74496738aa8bd8275c1a13b88a40a9de2177e
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-14 19:18:47 +0100

    general refactoring

--- com/ikinsure/hyperskill/hard/sorting/Input.java
+++ com/ikinsure/hyperskill/hard/sorting/Input.java
@@ -1,62 +1,31 @@
 package com.ikinsure.hyperskill.hard.sorting;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Input {
 
     private static final Scanner SCANNER = new Scanner(System.in);;
-    private static Input instance;
 
-    private Input() { }
-
-    public static Input getInstance() {
-        if (instance == null) {
-            instance = new Input();
-        }
-        return instance;
-    }
-
-    protected Map<String, String> createMapFromArgs(String[] args) {
-        LinkedHashMap<String, String> commands = new LinkedHashMap<>();
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].startsWith("-")) {
-                if (args.length - 1 > i && !args[i + 1].startsWith("-")) {
-                    commands.put(args[i], args[i + 1]);
-                    i++;
-                } else {
-                    commands.put(args[i], null);
-                }
-            }
-        }
-        return commands;
-    }
-
-    protected List<Long> dataTypeLong() {
-        ArrayList<Long> list = new ArrayList<>();
-        while (SCANNER.hasNext()) {
-            String in = SCANNER.next();
-            if (in.matches("-?\\d+")) {
-                list.add(Long.parseLong(in));
-            } else {
-                System.out.println("\"" + in + "\" is not a long. It will be skipped.");
-            }
-        }
-        return list;
-    }
-
-    protected List<String> dataTypeWord() {
-        ArrayList<String> list = new ArrayList<>();
-        while (SCANNER.hasNext()) {
-            list.add(SCANNER.next());
-        }
-        return list;
-    }
-
-    protected List<String> dataTypeLine() {
+    static List<String> readConsole() {
         ArrayList<String> list = new ArrayList<>();
         while (SCANNER.hasNextLine()) {
             list.add(SCANNER.nextLine());
         }
         return list;
+    }
+
+    static void write(String fileName, String data) throws IOException {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(data);
+        }
+    }
+
+    static List<String> readFile(String fileName) throws IOException {
+        return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
     }
 }
--- com/ikinsure/hyperskill/hard/sorting/Main.java
+++ com/ikinsure/hyperskill/hard/sorting/Main.java
@@ -1,4 +1,5 @@
 package com.ikinsure.hyperskill.hard.sorting;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.*;
 
@@ -6,64 +7,165 @@ public class Main {
 
     private static final DecimalFormat DECIMAL = new DecimalFormat("#");
 
-    public static void main(final String[] args) {
-        Map<String, String> commands = Input.getInstance().createMapFromArgs(args);
+    // commands
+    private static final String DATA = "dataType";
+    private static final String WORD = "word";
+    private static final String LINE = "line";
+    private static final String LONG = "long";
+    private static final String SORT = "sortingType";
+    private static final String NATURAL = "natural";
+    private static final String COUNT = "byCount";
+    private static final String INPUT = "inputFile";
+    private static final String OUTPUT = "outputFile";
 
-        String dataType = "word";
-        String sortingType = "natural";
+    public static void main(final String[] args) {
+
+        // get commands and prepare
+        Map<String, String> commands = createMapFromArgs(args);
+        String dataType = WORD;
+        String sortingType = NATURAL;
+        String inputFile = null;
+        String outputFile = null;
+
+        // check all commands
         for (var entry : commands.entrySet()) {
-            if (entry.getKey().equals("-dataType")) {
-                if (entry.getValue() == null) {
-                    System.out.println("No data type defined!");
-                    return;
-                } else {
-                    dataType = entry.getValue();
-                }
-            } else if (entry.getKey().equals("-sortingType")) {
-                if (entry.getValue() == null) {
-                    System.out.println("No sorting type defined!");
-                    return;
-                } else {
-                    sortingType = entry.getValue();
-                }
-            } else {
-                System.out.println("\"" + entry.getKey() + "\" is not a valid parameter. It will be skipped.");
+            switch (entry.getKey()) {
+                case DATA:
+                    if (entry.getValue() == null) {
+                        System.out.println("No data type defined!");
+                        return;
+                    } else {
+                        dataType = entry.getValue();
+                    }
+                    break;
+                case SORT:
+                    if (entry.getValue() == null) {
+                        System.out.println("No sorting type defined!");
+                        return;
+                    } else {
+                        sortingType = entry.getValue();
+                    }
+                    break;
+                case INPUT:
+                    if (entry.getValue() == null) {
+                        System.out.println("No input file defined!");
+                        return;
+                    } else {
+                        inputFile = entry.getValue();
+                    }
+                    break;
+                case OUTPUT:
+                    if (entry.getValue() == null) {
+                        System.out.println("No output file defined!");
+                        return;
+                    } else {
+                        outputFile = entry.getValue();
+                    }
+                    break;
+                default:
+                    System.out.println("\"" + entry.getKey() + "\" is not a valid parameter. It will be skipped.");
+                    break;
             }
         }
 
-        process(sortingType, dataType);
+        // process types
+        process(sortingType, dataType, inputFile, outputFile);
     }
 
-    private static void process(String sortingType, String dataType) {
-        if (sortingType.equals("natural") && dataType.equals("word")) {
-            sortNatural(Input.getInstance().dataTypeWord(), Comparator.naturalOrder(), " ");
-        } else if (sortingType.equals("natural") && dataType.equals("line")) {
-            sortNatural(Input.getInstance().dataTypeLine(), (o1, o2) -> o2.length() - o1.length(), "\n");
-        } else if (sortingType.equals("natural") && dataType.equals("long")) {
-            sortNatural(Input.getInstance().dataTypeLong(), Comparator.naturalOrder(), " ");
-        } else if (sortingType.equals("byCount") && dataType.equals("word")) {
-            sortByCount(Input.getInstance().dataTypeWord(), Comparator.naturalOrder());
-        } else if (sortingType.equals("byCount") && dataType.equals("line")) {
-            sortByCount(Input.getInstance().dataTypeLine(), Comparator.naturalOrder());
-        } else if (sortingType.equals("byCount") && dataType.equals("long")) {
-            sortByCount(Input.getInstance().dataTypeLong(), Comparator.naturalOrder());
+    private static void process(String sortingType, String dataType, String inputFile, String outputFile) {
+        String data = null;
+        List<String> list = null;
+        try {
+            list = inputFile == null
+                    ? Input.readConsole()
+                    : Input.readFile(inputFile);
+        } catch (IOException e) {
+            System.out.println("Error when reading " + inputFile);
+            return;
+        }
+        if (sortingType.equals(NATURAL) && dataType.equals(WORD)) {
+            data = sortNatural(lineToWord(list), Comparator.naturalOrder(), " ");
+        } else if (sortingType.equals(NATURAL) && dataType.equals(LINE)) {
+            data = sortNatural(list, (o1, o2) -> o2.length() - o1.length(), "\n");
+        } else if (sortingType.equals(NATURAL) && dataType.equals(LONG)) {
+            data = sortNatural(lineToLong(list), Comparator.naturalOrder(), " ");
+        } else if (sortingType.equals(COUNT) && dataType.equals(WORD)) {
+            data = sortByCount(lineToWord(list), Comparator.naturalOrder());
+        } else if (sortingType.equals(COUNT) && dataType.equals(LINE)) {
+            data = sortByCount(list, Comparator.naturalOrder());
+        } else if (sortingType.equals(COUNT) && dataType.equals(LONG)) {
+            data = sortByCount(lineToLong(list), Comparator.naturalOrder());
         } else {
             System.out.println("There is not sorting \"" + sortingType + "\" or data \"" + dataType + "\"");
+            return;
+        }
+        if (outputFile == null) {
+            System.out.println(data);
+        } else {
+            try {
+                Input.write(outputFile, data);
+            } catch (IOException e) {
+                System.out.println("Error when saving to " + outputFile);
+            }
         }
     }
 
-    private static <T> void sortByCount(List<T> list, Comparator<T> comparator) {
+    private static <T> String sortByCount(List<T> list, Comparator<T> comparator) {
         HashMultiSet<T> set = new HashMultiSet<>(list);
         set.sort(comparator);
-        System.out.println("Total: " + list.size() + ".");
-        set.getMap().forEach((k, v) ->
-                System.out.println(k + ": " + v + " time(s), " + DECIMAL.format(100.0 * v / list.size()) + "%"));
+        StringBuilder builder = new StringBuilder("Total: " + list.size() + ".\n");
+        set.getMap().forEach((k, v) -> builder
+                .append(k).append(": ")
+                .append(v).append(" time(s), ")
+                .append(DECIMAL.format(100.0 * v / list.size()))
+                .append("%\n"));
+        return builder.toString();
     }
 
-    private static <T> void sortNatural(List<T> list, Comparator<T> comparator, String separator) {
-        System.out.println("Total: " + list.size() + ".");
-        System.out.print("Sorted data:" + separator);
-        list.stream().sorted(comparator).forEach(i -> System.out.print(i + separator));
-        System.out.println();
+    private static <T> String sortNatural(List<T> list, Comparator<T> comparator, String separator) {
+        StringBuilder builder = new StringBuilder("Total: " + list.size() + ".\n");
+        builder.append("Sorted data:").append(separator);
+        list.stream().sorted(comparator).forEach(i -> builder
+                .append(i).append(separator));
+        builder.append("\n");
+        return builder.toString();
+    }
+
+    private static List<String> lineToWord(List<String> lines) {
+        ArrayList<String> result = new ArrayList<>();
+        for (String line : lines) {
+            result.addAll(Arrays.asList(line.split("\\s+")));
+        }
+        return result;
+    }
+
+    private static List<Long> lineToLong(List<String> lines) {
+        ArrayList<Long> result = new ArrayList<>();
+        for (String line : lines) {
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                if (word.matches("-?\\d+")) {
+                    result.add(Long.parseLong(word));
+                } else {
+                    System.out.println("\"" + word + "\" is not a long. It will be skipped.");
+                }
+            }
+        }
+        return result;
+    }
+
+    private static Map<String, String> createMapFromArgs(String[] args) {
+        LinkedHashMap<String, String> commands = new LinkedHashMap<>();
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                if (args.length - 1 > i && !args[i + 1].startsWith("-")) {
+                    commands.put(args[i].substring(1), args[i + 1]);
+                    i++;
+                } else {
+                    commands.put(args[i].substring(1), null);
+                }
+            }
+        }
+        return commands;
     }
 }
