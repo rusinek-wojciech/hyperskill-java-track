74544fa89f9744df9c40e2207b8412650a7089df
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-15 16:26:53 +0100

    calculator - stage 2/8

--- com/ikinsure/hyperskill/hard/calculator/Main.java
+++ com/ikinsure/hyperskill/hard/calculator/Main.java
@@ -7,8 +7,22 @@ public class Main {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
-        long a = SCANNER.nextLong();
-        long b = SCANNER.nextLong();
-        System.out.println(a + b);
+        while (true) {
+            String[] commands = SCANNER.nextLine().split("\\s+");
+            if (commands.length >= 2) {
+                long a = Long.parseLong(commands[0]);
+                long b = Long.parseLong(commands[1]);
+                System.out.println(a + b);
+            } else if (commands.length >= 1) {
+                if ("/exit".equals(commands[0])) {
+                    break;
+                } else {
+                    if (!commands[0].isBlank()) {
+                        System.out.println(commands[0]);
+                    }
+                }
+            }
+        }
+        System.out.println("Bye!");
     }
 }
