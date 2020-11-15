449106125be457684411449520ceecd851c4627a
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-15 16:14:22 +0100

    calculator - stage 1/8

---
+++ com/ikinsure/hyperskill/hard/calculator/Main.java
@@ -0,0 +1,14 @@
+package com.ikinsure.hyperskill.hard.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        long a = SCANNER.nextLong();
+        long b = SCANNER.nextLong();
+        System.out.println(a + b);
+    }
+}
