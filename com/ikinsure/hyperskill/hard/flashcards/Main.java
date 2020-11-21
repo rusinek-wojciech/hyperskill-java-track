91b11a5cf7d79cc34a0cde48def66233c440a590
iKinsure <59565620+iKinsure@users.noreply.github.com>
2020-11-21 18:52:28 +0100

    flashcards - stage 1/7 - init

---
+++ com/ikinsure/hyperskill/hard/flashcards/Main.java
@@ -0,0 +1,10 @@
+package com.ikinsure.hyperskill.hard.flashcards;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card:\n" +
+                "purchase\n" +
+                "Definition:\n" +
+                "buy");
+    }
+}
