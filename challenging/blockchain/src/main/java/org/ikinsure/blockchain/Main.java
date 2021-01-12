67f56b875d2b1710effa3d49e0b194eef028b330
iKinsure <59565620+iKinsure@users.noreply.github.com>
2021-01-12 21:36:51 +0100

    blockchain - stage 1/6

--- challenging/blockchain/src/main/java/org/ikinsure/blockchain/Main.java
+++ challenging/blockchain/src/main/java/org/ikinsure/blockchain/Main.java
@@ -2,6 +2,16 @@ package org.ikinsure.blockchain;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Block[] blockchain = new Block[10];
+        blockchain[0] = new Block("0");
+        for (int i = 1; i < blockchain.length; i++) {
+            blockchain[i] = new Block(blockchain[i - 1].getHash());
+        }
+
+        for (Block block : blockchain) {
+            System.out.println(block + "\n");
+        }
+
     }
 }
